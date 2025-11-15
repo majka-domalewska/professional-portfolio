cab1839be1d5cbb74cc9d0f2465feb745bc508d5
Maja Domalewska <162155232+majka-domalewska@users.noreply.github.com>
2025-11-16 00:44:16 +0100

    Just made the infinite scroll animation.
    Tutorial I used: https://cruip.com/create-an-infinite-horizontal-scroll-animation-with-tailwind-css/

--- portfolio/src/app/components/homepage/homepage.ts
+++ portfolio/src/app/components/homepage/homepage.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero/hero';
 import { BgGradient } from '../bg-gradient/bg-gradient';
+import { InfiniteScroll } from '../infinite-scroll/infinite-scroll';
 
 @Component({
   selector: 'app-homepage',
-  imports: [Hero, BgGradient],
+  imports: [Hero, BgGradient, InfiniteScroll],
   templateUrl: './homepage.html',
   styleUrl: './homepage.css',
 })
---
+++ portfolio/src/app/components/infinite-scroll/infinite-scroll.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-infinite-scroll',
+  imports: [],
+  templateUrl: './infinite-scroll.html',
+  styleUrl: './infinite-scroll.css',
+})
+export class InfiniteScroll {
+
+}
