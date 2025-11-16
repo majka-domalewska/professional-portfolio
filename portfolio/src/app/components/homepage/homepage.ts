b88d8359b55d5205b5bc5153b31e79aebd2f3187
Maja Domalewska <162155232+majka-domalewska@users.noreply.github.com>
2025-11-16 23:18:30 +0100

    Finished projects section and added assets for ICC and InnoVactions projects.

--- portfolio/src/app/components/homepage/homepage.ts
+++ portfolio/src/app/components/homepage/homepage.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { Hero } from '../hero/hero';
+import { Hero } from '../sections/hero-section/hero';
 import { BgGradient } from '../bg-gradient/bg-gradient';
 import { InfiniteScroll } from '../infinite-scroll/infinite-scroll';
 import { SectionName } from '../section-name/section-name';
+import { ProjectsSection } from '../sections/projects-section/projects-section';
 
 @Component({
   selector: 'app-homepage',
-  imports: [Hero, BgGradient, InfiniteScroll, SectionName],
+  imports: [Hero, BgGradient, InfiniteScroll, SectionName, ProjectsSection],
   templateUrl: './homepage.html',
   styleUrl: './homepage.css',
 })
