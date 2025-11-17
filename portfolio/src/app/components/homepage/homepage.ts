658f347cf1c25998b8e043a299066a099ff4e77c
Maja Domalewska <162155232+majka-domalewska@users.noreply.github.com>
2025-11-17 01:24:14 +0100

    Made the about me / contact section responsive and added a contact form.
    I know it's ugly, I promise I'll make it look better later... it's almost 2AM ;-;

--- portfolio/src/app/components/homepage/homepage.ts
+++ portfolio/src/app/components/homepage/homepage.ts
@@ -5,10 +5,11 @@ import { InfiniteScroll } from '../infinite-scroll/infinite-scroll';
 import { SectionName } from '../section-name/section-name';
 import { ProjectsSection } from '../sections/projects-section/projects-section';
 import { SkillsSection } from '../sections/skills-section/skills-section';
+import { Contact } from '../sections/contact-section/contact';
 
 @Component({
   selector: 'app-homepage',
-  imports: [Hero, BgGradient, InfiniteScroll, SectionName, ProjectsSection, SkillsSection],
+  imports: [Hero, BgGradient, InfiniteScroll, SectionName, ProjectsSection, SkillsSection, Contact],
   templateUrl: './homepage.html',
   styleUrl: './homepage.css',
 })
