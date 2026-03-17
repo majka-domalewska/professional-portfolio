c72902a79adbca2c3d7a69d5fa1a089f5470c323
Maja Domalewska <162155232+majka-domalewska@users.noreply.github.com>
2026-03-18 00:52:30 +0100

    I've updated the page to have dark and light mode available. I'm really proud, good comeback. I only didn't manage to change the color of the logo in the header, that's for later I guess. Good night world!

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { Homepage } from './components/homepage/homepage';
+import { AboutMe } from './components/about-me/about-me';
+import { NotFoundPage } from './components/not-found-page/not-found-page';
 
 export const routes: Routes = [
-     { path: '', component: Homepage },
-     {  path: '**', redirectTo: '' }
+    { path: 'home', component: Homepage, pathMatch: 'full' },
+    { path: 'aboutme', component: AboutMe },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: '**', component: NotFoundPage },
 ];
--- portfolio/src/app/components/homepage/homepage.ts
+++ portfolio/src/app/components/homepage/homepage.ts
@@ -7,10 +7,11 @@ import { ProjectsSection } from '../sections/projects-section/projects-section';
 import { SkillsSection } from '../sections/skills-section/skills-section';
 import { Contact } from '../sections/contact-section/contact';
 import { Footer } from '../footer/footer';
+import { Header } from '../header/header';
 
 @Component({
   selector: 'app-homepage',
-  imports: [Hero, BgGradient, InfiniteScroll, SectionName, ProjectsSection, SkillsSection, Contact, Footer],
+  imports: [Header, Hero, BgGradient, InfiniteScroll, SectionName, ProjectsSection, SkillsSection, Contact, Footer],
   templateUrl: './homepage.html',
   styleUrl: './homepage.css',
 })
